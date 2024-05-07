a08c10cf97e4802b382c32fe2d9aca4dfc901d64
André Reis <andrecar95@outlook.com>
2024-05-06 21:55:56 -0300

    GroundChecker

---
+++ Assets/Scripts/PhysicsBasedMovement/CollisionChecker.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+using System.Collections;
+using System.Reflection;
+using Unity.Mathematics;
+using UnityEditor.ShaderGraph.Drawing;
+using UnityEngine.EventSystems;
+
+public class CollisionChecker : RaycastController {
+    public CollisionInfo collisions;
+
+    public float checkDistance = 0.5f;
+
+    public override void Start() {
+        base.Start();
+        collisions.verticalTag = new string[verticalRayCount];
+    }
+
+    public void CheckGrounder(Vector3 currentSpeed) {
+        Vector2 aux = Vector2.zero;
+        collisions.Reset();
+        VerticalCollisions(currentSpeed);
+    }
+
+    void VerticalCollisions(Vector3 currentSpeed) {
+        UpdateRaycastOrigins();
+
+
+        float directionY = currentSpeed.y == 0 ? -1 : Mathf.Sign(currentSpeed.y);
+
+
+        float rayLength = checkDistance;
+
+
+        for (int i = 0; i < verticalRayCount; i++) {
+
+            Vector2 rayOrigin = (directionY == -1) ? raycastOrigins.bottomLeft : raycastOrigins.topLeft;
+            rayOrigin += Vector2.right * (verticalRaySpacing * i + currentSpeed.x * Time.deltaTime);
+            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up * directionY, rayLength, collisionMask);
+
+            Debug.DrawRay(rayOrigin, Vector2.up * directionY * rayLength, Color.red);
+
+            if (hit) {
+                //rayLength = hit.distance;
+
+                collisions.below = directionY == -1;
+                collisions.above = directionY == 1;
+
+                collisions.verticalTag[i] = hit.collider.tag;
+            }
+            else {
+                collisions.verticalTag[i] = null;
+            }
+        }
+    }
+
+    public struct CollisionInfo {
+        public bool above, below;
+        public bool left, right;
+
+        public string[] verticalTag;
+
+        public void Reset() {
+            above = below = false;
+            left = right = false;
+        }
+    }
+
+}
---
+++ Assets/Scripts/PhysicsBasedMovement/RaycastController.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class RaycastController : MonoBehaviour {
+
+    public LayerMask collisionMask;
+
+    public const float skinWidth = .015f;
+
+    const float dstBetweenRays = .25f;
+
+    public int verticalRayCount;
+
+    [HideInInspector]
+    public float verticalRaySpacing;
+
+    BoxCollider2D collider;
+    public RaycastOrigins raycastOrigins;
+
+    public virtual void Awake() {
+        collider = this.GetComponent<BoxCollider2D>();
+    }
+
+    public virtual void Start() {
+        CalculateRaySpacing();
+    }
+
+    public void UpdateRaycastOrigins() {
+        Bounds bounds = collider.bounds;
+        bounds.Expand(skinWidth * -2);
+
+        raycastOrigins.bottomLeft = new Vector2(bounds.min.x, bounds.min.y);
+        raycastOrigins.bottomRight = new Vector2(bounds.max.x, bounds.min.y);
+        raycastOrigins.topLeft = new Vector2(bounds.min.x, bounds.max.y);
+        raycastOrigins.topRight = new Vector2(bounds.max.x, bounds.max.y);
+    }
+
+    public void CalculateRaySpacing() {
+        Bounds bounds = collider.bounds;
+        bounds.Expand(skinWidth * -2);
+
+        float boundsWidth = bounds.size.x;
+
+        if (verticalRayCount <= 0) {
+            verticalRayCount = Mathf.RoundToInt(boundsWidth / dstBetweenRays);
+        }
+        verticalRaySpacing = bounds.size.x / (verticalRayCount - 1);
+    }
+
+    public struct RaycastOrigins {
+        public Vector2 topLeft, topRight;
+        public Vector2 bottomLeft, bottomRight;
+    }
+}
