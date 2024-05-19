18b9822577f7ac04372b175e169b95cf9a7498e6
panzenhagen <gabriel.panzenhagen@gmail.com>
2024-05-19 14:06:51 -0300

    fix: apaga checkpoint ao trocar de cena

--- Assets/Scripts/LevelObjective.cs
+++ Assets/Scripts/LevelObjective.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class LevelObjective : Collectable {
     protected override void CollectableEffect() {
+        GameController.Checkpoint = Vector2.zero;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
