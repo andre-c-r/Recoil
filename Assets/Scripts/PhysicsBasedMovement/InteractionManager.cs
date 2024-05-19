4580fd792e8251e405da7a47312ad5dc754f260d
Felipe Mendonça <felipe.atena.daniel@gmail.com>
2024-05-19 15:36:32 -0300

    Added Script on collider spawn dust, added dust prefab

--- Assets/Scripts/PhysicsBasedMovement/InteractionManager.cs
+++ Assets/Scripts/PhysicsBasedMovement/InteractionManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class InteractionManager : MonoBehaviour {
     Player player;
+    public ParticleSystem dust;
+    public static Vector2 Checkpoint { get; set; } = Vector2.zero;
 
     private void Start() {
         player = this.GetComponent<Player>();
@@ -13,12 +15,24 @@ public class InteractionManager : MonoBehaviour {
         if (collision.collider == null) return;
 
         if (collision.collider.CompareTag("Enemy")) {
-            // GameController.Singleton?.ResetLevel();
-            GameController.Singleton?.ResetToCheckpoint();
+            GameController.Singleton?.ResetLevel();
         }
 
         if (collision.collider.CompareTag("RubberFloor")) {
             player?.ReflectSpeed(collision.GetContact(0).normal);
         }
+
+        if (collision.collider.CompareTag("Floor")) {
+            Vector2 contactPoint = collision.GetContact(0).point;
+            CreateDust(contactPoint);
+        }
+
     }
+    private void CreateDust(Vector2 position) {
+        // Convert Vector2 position to Vector3 to match the ParticleSystem's transform position requirement
+        Vector2 dustPosition = new Vector2(position.x, position.y);
+        dust.transform.position = dustPosition; // Move the dust particle system to the contact point
+        dust.Play(); // Play the particle effect
+    }
+
 }
